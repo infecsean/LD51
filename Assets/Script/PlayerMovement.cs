b19c51c8b2a7a33157e6c2df6b630bbbe691c254
infecsean <72145138+infecsean@users.noreply.github.com>
2022-10-01 20:05:12 +0800

    Added player movement

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform player;
+    public GameObject spawnPoint;
+    public float walkSpeed;
+
+    private float movementX;
+    private float movementY;
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1)
+        {
+            player.position += new Vector3(Input.GetAxisRaw("Horizontal") * walkSpeed, 0f, 0f);
+            UpdateRotation();
+        }
+        else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1)
+        {
+            player.position += new Vector3(0f, Input.GetAxisRaw("Vertical") * walkSpeed, 0f);
+        }
+
+        Debug.Log(Input.GetAxis("Horizontal") + ", " + Input.GetAxis("Vertical"));
+
+    }
+
+    private void UpdateRotation()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            player.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            player.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else
+            return;
+    }
+
+}
