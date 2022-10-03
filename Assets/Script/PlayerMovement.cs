6968235addd5cf87a93da2a7271ca19bf904e40b
Alb33rt <albert931023@gmail.com>
2022-10-03 10:21:00 +0800

    Add Player Animations

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -6,46 +6,42 @@ public class PlayerMovement : MonoBehaviour
 {
     public Transform player;
     public float walkSpeed;
-    
 
+    public Animator animator;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        walkSpeed /= 100;
     }
 
     // Update is called once per frame
     void Update()
     {
+        animator.SetFloat("Speed", (Mathf.Abs(Input.GetAxisRaw("Horizontal")) + Mathf.Abs(Input.GetAxisRaw("Vertical"))));
+        animator.SetInteger("XDirection", (int)Input.GetAxisRaw("Horizontal"));
 
-        if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1)
+        if (Input.GetAxisRaw("Horizontal") == 0)
+        {
+            animator.SetInteger("YDirection", (int)Input.GetAxisRaw("Vertical"));
+        } 
+        else
+        {
+            animator.SetInteger("YDirection", 0);
+        }
+
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1)
         {
             player.position += new Vector3(Input.GetAxisRaw("Horizontal") * walkSpeed, 0f, 0f);
-            UpdateRotation();
         }
         else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1)
         {
             player.position += new Vector3(0f, Input.GetAxisRaw("Vertical") * walkSpeed, 0f);
         }
+        
 
         //Debug.Log(Input.GetAxis("Horizontal") + ", " + Input.GetAxis("Vertical"));
 
     }
-
-    private void UpdateRotation()
-    {
-        if (Input.GetAxisRaw("Horizontal") > 0)
-        {
-            player.GetComponent<SpriteRenderer>().flipX = true;
-        }
-        else if (Input.GetAxisRaw("Horizontal") < 0)
-        {
-            player.GetComponent<SpriteRenderer>().flipX = false;
-        }
-        else
-            return;
-    }
-
 }
